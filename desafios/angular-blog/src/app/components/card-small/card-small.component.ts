6b9ddea4f07cb0b99f6663828bce24771bb1e1af
Elizabete Fabri <elizabete958@gmail.com>
2023-09-08 20:28:37 -0300

    css components

--- desafios/angular-blog/src/app/components/card-small/card-small.component.ts
+++ desafios/angular-blog/src/app/components/card-small/card-small.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-small',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-small.component.css']
 })
 export class CardSmallComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
 
   constructor() { }
 
